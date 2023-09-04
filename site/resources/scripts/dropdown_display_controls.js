623cf65ba227e1d23250dd2708758f13c9882bdc
PencilVoid <musty.yew@gmail.com>
2023-09-04 18:25:26 +0100

    Make display controls collapsible

---
+++ site/resources/scripts/dropdown_display_controls.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+	let dropdownDisplayControls = sessionStorage.getItem("dropdown-display-controls");
+
+	if (dropdownDisplayControls) {
+		if (dropdownDisplayControls === "true") {
+			document.getElementById("dropdown-display-controls").checked = true
+		} else if (dropdownDisplayControls === "false") {
+			document.getElementById("dropdown-display-controls").checked = false
+		}
+	} else {
+		document.getElementById("dropdown-display-controls").checked = true
+	}
+})
+
+document.getElementById("dropdown-display-controls").addEventListener("change", function(event) {
+	if (document.getElementById("dropdown-display-controls").checked) {
+		sessionStorage.setItem("dropdown-display-controls", "true");
+	} else {
+		sessionStorage.setItem("dropdown-display-controls", "false");
+	}
+})
