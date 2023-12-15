75e406491b14056f1d3a322525120ee44df5819d
PencilVoid <pencilvoid@gmail.com>
2023-12-15 23:21:10 +0000

    Add simple background option

--- site/resources/scripts/display_bg_checker.js
+++ site/resources/scripts/display_bg_checker.js
@@ -3,7 +3,7 @@ function bgCheckerVisible() {
 }
 
 function bgCheckerInvisible() {
-	document.body.style.backgroundImage = "linear-gradient(to bottom left, var(--bg-checkerdark,#42004E) 0%, var(--bg-checkerdark,#42004E) 50%, var(--bg-triangle,#220029) 50%, var(--bg-triangle,#220029) 100%), linear-gradient(45deg, var(--bg-checkerlight,#680068) 25%, transparent 25%, transparent 75%, var(--bg-checkerlight,#680068) 75%, var(--bg-checkerlight,#680068)), linear-gradient(45deg, var(--bg-checkerlight,#680068) 25%, var(--bg-checkerdark,#42004E) 25%, var(--bg-checkerdark,#42004E) 75%, var(--bg-checkerlight,#680068) 75%, var(--bg-checkerlight,#680068))";
+	document.body.style.backgroundImage = "linear-gradient(to bottom left, var(--bg-checkerdark,#42004E) 0%, var(--bg-checkerdark,#42004E) 100%), linear-gradient(45deg, var(--bg-checkerlight,#680068) 25%, transparent 25%, transparent 75%, var(--bg-checkerlight,#680068) 75%, var(--bg-checkerlight,#680068)), linear-gradient(45deg, var(--bg-checkerlight,#680068) 25%, var(--bg-checkerdark,#42004E) 25%, var(--bg-checkerdark,#42004E) 75%, var(--bg-checkerlight,#680068) 75%, var(--bg-checkerlight,#680068))";
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -12,25 +12,25 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	if (cookieDisplayBgChecker) {
 		if (cookieDisplayBgChecker === "false") {
 			bgCheckerInvisible();
-			if (document.getElementById("display-bg-checker")) { document.getElementById("display-bg-checker").checked = false; }
+			if (document.getElementById("display-bg-checker")) { document.getElementById("display-bg-checker").checked = true; }
 		} else if (cookieDisplayBgChecker === "true") {
 			bgCheckerVisible();
-			if (document.getElementById("display-bg-checker")) { document.getElementById("display-bg-checker").checked = true; }
+			if (document.getElementById("display-bg-checker")) { document.getElementById("display-bg-checker").checked = false; }
 		}
 	} else {
 		bgCheckerVisible();
-		if (document.getElementById("display-bg-checker")) { document.getElementById("display-bg-checker").checked = true; }
+		if (document.getElementById("display-bg-checker")) { document.getElementById("display-bg-checker").checked = false; }
 	}
 })
 
 if (document.getElementById("display-bg-checker")) {
 	document.getElementById("display-bg-checker").addEventListener("change", function(event) {
 		if (document.getElementById("display-bg-checker").checked) {
-			bgCheckerVisible();
-			localStorage.setItem("display_bg_checker", "true");
-		} else {
 			bgCheckerInvisible();
 			localStorage.setItem("display_bg_checker", "false");
+		} else {
+			bgCheckerVisible();
+			localStorage.setItem("display_bg_checker", "true");
 		}
 	})
 }
