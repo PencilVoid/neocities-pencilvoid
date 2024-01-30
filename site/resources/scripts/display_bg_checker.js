b74d0126cb7385ec245763dc44f2de13cf261e80
PencilVoid <pencilvoid@gmail.com>
2024-01-30 08:30:25 +0000

    Fix simple background option

--- site/resources/scripts/display_bg_checker.js
+++ site/resources/scripts/display_bg_checker.js
@@ -1,9 +1,19 @@
 function bgCheckerVisible() {
-	document.body.style.backgroundImage = "linear-gradient(to bottom left, transparent 0%, transparent 50%, var(--bg-triangle,#220029) 50%, var(--bg-triangle,#220029) 100%), linear-gradient(45deg, var(--bg-checkerlight,#680068) 25%, transparent 25%, transparent 75%, var(--bg-checkerlight,#680068) 75%, var(--bg-checkerlight,#680068)), linear-gradient(45deg, var(--bg-checkerlight,#680068) 25%, var(--bg-checkerdark,#42004E) 25%, var(--bg-checkerdark,#42004E) 75%, var(--bg-checkerlight,#680068) 75%, var(--bg-checkerlight,#680068))"
+	document.getElementsByClassName("body-grid-left")[0].style.backgroundImage = `linear-gradient(to bottom left, transparent 0%, transparent 50%, var(--bg-triangle,#220029) 50%, var(--bg-triangle,#220029) 100%),
+	linear-gradient(45deg, var(--bg-checkerlight,#680068) 25%, transparent 25%, transparent 75%, var(--bg-checkerlight,#680068) 75%, var(--bg-checkerlight,#680068)),
+	linear-gradient(45deg, var(--bg-checkerlight,#680068) 25%, var(--bg-checkerdark,#42004E) 25%, var(--bg-checkerdark,#42004E) 75%, var(--bg-checkerlight,#680068) 75%, var(--bg-checkerlight,#680068))`;
+	document.getElementsByClassName("body-grid-right")[0].style.backgroundImage = `background-image: linear-gradient(to bottom left, transparent 0%, transparent 50%, var(--bg-triangle,#220029) 50%, var(--bg-triangle,#220029) 100%),
+	linear-gradient(45deg, var(--bg-checkerlight,#680068) 25%, transparent 25%, transparent 75%, var(--bg-checkerlight,#680068) 75%, var(--bg-checkerlight,#680068)),
+	linear-gradient(45deg, var(--bg-checkerlight,#680068) 25%, var(--bg-checkerdark,#42004E) 25%, var(--bg-checkerdark,#42004E) 75%, var(--bg-checkerlight,#680068) 75%, var(--bg-checkerlight,#680068))`;
 }
 
 function bgCheckerInvisible() {
-	document.body.style.backgroundImage = "linear-gradient(to bottom left, var(--bg-checkerdark,#42004E) 0%, var(--bg-checkerdark,#42004E) 100%), linear-gradient(45deg, var(--bg-checkerlight,#680068) 25%, transparent 25%, transparent 75%, var(--bg-checkerlight,#680068) 75%, var(--bg-checkerlight,#680068)), linear-gradient(45deg, var(--bg-checkerlight,#680068) 25%, var(--bg-checkerdark,#42004E) 25%, var(--bg-checkerdark,#42004E) 75%, var(--bg-checkerlight,#680068) 75%, var(--bg-checkerlight,#680068))";
+	document.getElementsByClassName("body-grid-left")[0].style.backgroundImage = `linear-gradient(to bottom left, transparent 0%, transparent 100%),
+	linear-gradient(45deg, var(--bg-checkerdark,#42004E) 25%, transparent 25%, transparent 75%, var(--bg-checkerdark,#42004E) 75%, var(--bg-checkerdark,#42004E)),
+	linear-gradient(45deg,var(--bg-checkerdark,#42004E) 25%, var(--bg-checkerdark,#42004E) 25%, var(--bg-checkerdark,#42004E) 75%, var(--bg-checkerdark,#42004E) 75%, var(--bg-checkerdark,#42004E))`;
+	document.getElementsByClassName("body-grid-right")[0].style.backgroundImage = `linear-gradient(to bottom left, transparent 0%, transparent 100%),
+	linear-gradient(45deg, var(--bg-checkerdark,#42004E) 25%, transparent 25%, transparent 75%, var(--bg-checkerdark,#42004E) 75%, var(--bg-checkerdark,#42004E)),
+	linear-gradient(45deg,var(--bg-checkerdark,#42004E) 25%, var(--bg-checkerdark,#42004E) 25%, var(--bg-checkerdark,#42004E) 75%, var(--bg-checkerdark,#42004E) 75%, var(--bg-checkerdark,#42004E))`;
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
