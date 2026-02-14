b1bb1403c06ed2f0a0a0fdf67c8180825082029b
PencilVoid <pencilvoid@gmail.com>
2026-02-14 12:43:02 +0000

    Update games page, add UPAG and HyperMarble

---
+++ source/games/c2/hyperMarble/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1770851051,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/marble-sheet0.png",
+		"images/marble-sheet1.png",
+		"images/woodplanks.png",
+		"images/marbleoverlay-sheet0.png",
+		"images/dashzone-sheet0.png",
+		"images/toback-sheet0.png",
+		"images/woodplanksback.png",
+		"images/tofront-sheet0.png",
+		"images/dashzoneback-sheet0.png",
+		"images/widewoodplanks.png",
+		"images/corner-sheet0.png",
+		"images/cornerback-sheet0.png",
+		"images/layertoggle-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
